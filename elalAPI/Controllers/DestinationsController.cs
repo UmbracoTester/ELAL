d8fb5c9144186b0d70bf465c418e93fa50eb4cb0
Dor Danai <dordan@matrix.co.il>
2020-02-17 10:51:40 +0200

    Added published models, added DestinationsController that returns json of destinations

---
+++ elalAPI/Controllers/DestinationsController.cs
@@ -0,0 +1,47 @@
+﻿using elalAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Umbraco.Web.PublishedModels;
+using Umbraco.Web.WebApi;
+
+namespace elalAPI.Controllers
+{
+    public class DestinationsController : UmbracoApiController
+    {
+
+        [System.Web.Http.HttpGet]
+        public string DestinationById(int id ) {
+
+            //http://elal.test/umbraco/api/destinations/destinationbyid?id=1077
+            var ipc = UmbracoContext.Content.GetById(id);
+            FlightDestinationModel destination = new FlightDestinationModel(ipc);
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            return js.Serialize(destination);
+            
+        }
+
+        [System.Web.Http.HttpGet]
+        public string GetAllDestinations() {
+            //http://elal.test/umbraco/api/destinations/GetAllDestinations
+
+            var ipc = UmbracoContext.Content.GetByRoute("/he/all-destinations/");
+            List<FlightDestinationModel> destinations = new List<FlightDestinationModel>();
+            foreach (FlightDesination item in ipc.Children)
+            {
+                FlightDestinationModel dest = new FlightDestinationModel(item);
+                destinations.Add(dest);
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            return js.Serialize(destinations);
+
+        }
+
+
+
+    }
+}
---
+++ elalAPI/Models/FlightDestination.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.PublishedModels;
+
+namespace elalAPI.Models
+{
+    public class FlightDestinationModel
+    {
+        public string Airport { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public bool DestinationIsActive { get; set; }
+        public string DisplayName { get; set; }
+        public bool DisplayOnFlightPackages { get; set; }
+        public bool DisplayOnSearchEngine { get; set; }
+        public bool DisplayOnVehiclePackages { get; set; }
+        public string IAta { get; set; }
+        public bool ShowInFooter { get; set; }
+
+        public FlightDestinationModel(IPublishedContent ipc)
+        {
+            if (ipc is FlightDesination) { AssignProps(ipc as FlightDesination); }
+
+        }
+
+        private void AssignProps(FlightDesination fd)
+        {
+            Airport = fd.Airport;
+            City = fd.City;
+            Country = fd.Country;
+            DestinationIsActive = fd.DestinationIsActive;
+            DisplayName = fd.DisplayName;
+            DisplayOnFlightPackages = fd.DisplayOnFlightPackages;
+            DisplayOnSearchEngine = fd.DisplayOnSearchEngine;
+            DisplayOnVehiclePackages = fd.DisplayOnVehiclePackages;
+            IAta = fd.IAta;
+            ShowInFooter = fd.ShowInFooter;
+        }
+    }
+}
